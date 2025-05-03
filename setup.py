56a6c546b931a979c3f991bd60cacd0e859e0560
Ashutosh Singh <ashutosh100singh100@gmail.com>
2025-05-03 12:09:59 +0530

    Folder Structure Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Ashutosh Singh',
+    author_email = 'ashutosh100singh100@gmail.com',
+    packages = find_packages(),
+    install_requires = [],
+)
